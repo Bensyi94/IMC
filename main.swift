df3455e615a0379da03e203a4fe103800f4cc14b
HANS ALEXANDER JÜRGEN ALEXANDER <hbehrensg@alumnos.ceduc.cl>
2021-01-04 18:57:27 +0000

    2.0

--- main.swift
+++ main.swift
@@ -1,22 +1,41 @@
-print("Bienvenido/nIngrese su nombre:")
+import Glibc
+
+print("Bienvenido\nIngrese su nombre:")
 var nombre = readLine()!
-print("IMC CALCULATOR\nIngrese su peso:")
-var peso = Float(readLine()!)!
-print("Ingrese su altura:")
-var altura = Float(readLine()!)!
-var imc = peso/(altura*altura)
-print(imc)
-if imc < 18.5{
+print()
+var peso : Float = 0
+var altura : Float = 0
+var imc : Float = 0
+
+func catchInput(){
+    print("IMC CALCULATOR\nIngrese su peso:")
+    peso = Float(readLine()!)!
+    print("Ingrese su altura:")
+    altura = Float(readLine()!)!
+    imc = peso/(altura*altura)
+    outputMessage()
+}
+
+func outputMessage(){
+  if imc < 18.5 && imc > 0{
   print("\(nombre),\nSu IMC es de: \(imc)\nLo que indica que esta bajo peso")
-}else{
-  if imc >= 18.5 && imc <= 24.9{
-    print("\(nombre),\nSu IMC es de: \(imc)\nLo que indica que esta dentro de los valores normales ")
-  }
-  if imc >= 25 && imc <= 29.9{
-    print("\(nombre),\nSu IMC es de: \(imc)\nLo que indica que tiene sobrepeso")
-  }if imc > 30{
-    print("\(nombre),\nSu IMC es de: \(imc)\nLo que indica que padece de obesidad")
   }else{
-    print("Verifica los datos ingresados")
+    if imc >= 18.5 && imc <= 24.9{
+      print("\(nombre),\nSu IMC es de: \(imc)\nLo que indica que esta dentro de los valores normales ")
+    }
+    else if imc >= 25 && imc <= 29.9{
+      print("\(nombre),\nSu IMC es de: \(imc)\nLo que indica que tiene sobrepeso")
+    }
+    else if imc > 30{
+      print("\(nombre),\nSu IMC es de: \(imc)\nLo que indica que padece de obesidad")
+    }else{
+      print("\n\(nombre),\nVerifica los datos ingresados\nPeso: \(peso)\nAltura\(altura)\nPRESIONA ENTER")
+      readLine()!
+      system("clear")
+      print("Bienvenido \(nombre),")
+      catchInput()
+    }
   }
 }
+
+catchInput()
